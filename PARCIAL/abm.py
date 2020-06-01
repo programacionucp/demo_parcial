aa77180e3368d15457e5f9a2ce22d78a73b6abb7
agusfsa <63015006+agusfsa@users.noreply.github.com>
2020-06-01 16:31:31 -0300

    Ultimo requeriemiento terminado, abm

--- PARCIAL/abm.py
+++ PARCIAL/abm.py
@@ -50,6 +50,39 @@ def viajesPorOrigenYDestino(origen,destino):
         i+=1
     return varAux
 
+def viajesDiputados(diputado):
+    varAux = 0
+    i = 1
+    for elemento in listaViajes:
+        i = str(i)
+        if diputado in elemento[i]["Persona_apellido_y_nombre"]:
+            varAux+=1
+        i = int (i)
+        i+=1
+    return varAux
 
+def buscadorDiputados(id):
+    listAux = list()
+    i = 1
+    año = ""
+    for elemento in listaDiputados:
+        i= str(i)
+        if elemento[i]['﻿diputado_id'] == id:
+            apellido = elemento[i]['diputado_apellido']
+            nombre = elemento[i]['diputado_nombre']
+            completo = apellido +" "+nombre
+            listAux.append(completo)
+            listAux.append(elemento[i]['diputado_distrito'])
+            listAux.append(elemento[i]['diputado_bloque'])
+            inicioFecha = elemento[i]['mandato_inicio']
+            for e in range (4):
+                año += inicioFecha[e]
+            listAux.append(año)
+            listAux.append(viajesDiputados(nombre.upper()))
+            return listAux
+        i = int (i)
+        i+=1
+    return None
 
+print(buscadorDiputados("HCDN1136"))
 
