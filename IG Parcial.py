b6fd229cbf05d4db104462b9660f60378b952fe4
LuisV192 <63015181+LuisV192@users.noreply.github.com>
2020-06-01 16:05:39 -0300

    Parcial

--- IG Parcial.py
+++ IG Parcial.py
@@ -1,4 +1,12 @@
 from tkinter import *
+import abmDiputados
+
+def CantidadDeDiputados():
+    provincia = e1.get()
+    info = abmDiputados.CantidadDiputados(provincia)
+    print(info)
+
+
 
 vp = Tk()
 vp.geometry("900x650")
@@ -7,9 +15,9 @@ vp.title("Sistema Diputados")
 #primera opcion
 l1 = Label(vp, text="Cantidad de diputados por provincia ",font=("Arial",12)).place(x=10,y=10)
 l2 = Label(vp, text="Provincia:",font=("Arial",12)).place(x=10,y=60)
-e1 = Entry(vp, width=15)
-e1.place(x=100,y=60)
-btn1 = Button(vp, text="Calcular", font=("Arial",12)).place(x=250,y=55)
+e1 = Entry(vp, width=25)
+e1.place(x=90,y=60)
+btn1 = Button(vp, text="Calcular", font=("Arial",12),command=CantidadDeDiputados).place(x=250,y=55)
 l3 = Label(vp, text="Cantidad:",font=("Arial",12)).place(x=350,y=60)
 e2 = Entry(vp, width=15)
 e2.place(x=440,y=60)
@@ -28,7 +36,39 @@ e5 = Entry(vp, width=15)
 e5.place(x=440,y=180)
 
 #tercera opcion
+l8 = Label(vp, text="Cantidad de viajes",font=("Arial",12)).place(x=10,y=240)
+l9 = Label(vp, text="Origen:",font=("Arial",12)).place(x=10,y=280)
+e6 = Entry(vp, width=15)
+e6.place(x=90,y=280)
+l10 = Label(vp, text="Destino:",font=("Arial",12)).place(x=10,y=310)
+e7 = Entry(vp, width=15)
+e7.place(x=90,y=310)
+btn3 = Button(vp, text="Calcular", font=("Arial",12)).place(x=250,y=290)
+l11 = Label(vp, text="Cantidad:",font=("Arial",12)).place(x=350,y=290)
+e8 = Entry(vp, width=15)
+e8.place(x=440,y=290)
 
+#cuarta opcion
+l12 = Label(vp, text="Buscador de diputados",font=("Arial",12)).place(x=10,y=390)
+l13 = Label(vp, text="Diputado ID:",font=("Arial",12)).place(x=10,y=430)
+e9 = Entry(vp, width=15)
+e9.place(x=150,y=430)
+l14 = Label(vp, text="Nombre completo:",font=("Arial",12)).place(x=10,y=480)
+e10 = Entry(vp, width=15)
+e10.place(x=150,y=480)
+l15 = Label(vp, text="Provincia:",font=("Arial",12)).place(x=10,y=530)
+e11 = Entry(vp, width=15)
+e11.place(x=150,y=530)
+btn4 = Button(vp, text="Buscar", font=("Arial",12)).place(x=280,y=420)
 
+l16 = Label(vp, text="Bloque:",font=("Arial",12)).place(x=380,y=480)
+e12 = Entry(vp, width=15)
+e12.place(x=520,y=480)
+l17 = Label(vp, text="Año Inicio:",font=("Arial",12)).place(x=380,y=520)
+e13 = Entry(vp, width=15)
+e13.place(x=520,y=520)
+l18 = Label(vp, text="Cantidad de viajes:",font=("Arial",12)).place(x=380,y=560)
+e14 = Entry(vp, width=15)
+e14.place(x=520,y=560)
 
 vp,mainloop()
---
+++ abmDiputados.py
@@ -0,0 +1,21 @@
+import json
+
+with open('diputados.json', encoding="utf-8") as archivodipu:
+    listadiputados = json.load(archivodipu)
+
+with open('viajes.json', encoding="utf-8") as archivoviaje:
+    listaviajes = json.load(archivoviaje)
+
+def getDiputados():
+    return listadiputados
+
+def getViajes():
+    return listaviajes
+
+def CantidadDiputados(nombre):
+    cantidadDiputados = 0
+    for unDiputado in listadiputados:
+        infoDiputado = unDiputado['1']
+        if infoDiputado["diputado_distrito"]==nombre:
+            cantidadDiputados += 1
+    return cantidadDiputados
